a84cafd3b0eadae6e5f70fe363bf9fc1c350e88c
oranmore <mjkelly@ntlworld.com>
2022-01-26 16:36:29 +0000

    Create sshd.pp

---
+++ site/profile/manifests/sshd.pp
@@ -0,0 +1,15 @@
+class profile::ssh_server {
+        package {'openssh-server':
+                ensure => present,
+        }
+        service { 'sshd':
+                ensure => 'running',
+                enable => 'true',
+        }
+        ssh_authorized_key { 'root@master.puppet.vm':
+                ensure => present,
+                user   => 'root',
+                type   => 'ssh-rsa',
+                key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDiJASILl2BXkQdaf9xxmdcUzb11zSWvqveDjUNtNzDha9flxp/HYhCBXeyqCZxRzMRIEJ4KgfeF5QD8PG8IwIlPR6dIBch1HpjR/+xqtw0Yz7Ge6KaKZWjL6aQUFkzWucIQRjRgGMk4kdBneur0nEZphRIANi1ikZq3QrlPOKP/iOSAEieNx/E04U7YI0hyyDz3u3lpfmZRQvk6zVL7JMrpz3nH3bN6QvUNuc8xNRr4UDvZlzHlrVri3rz3mBznKvipckSTeuaa6C0bln5qRgDjHdTmOqrpZRfKRsN2A5qpqmN2VqqviBTXtZm2oAsk9+NUV0lVEGAWMVIeWMHTZ/z root@master.puppet.vm',
+        }
+}
